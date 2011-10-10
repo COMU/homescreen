de136dba84225dc0ba8f471c76dc5c94f40fee84
aysegovdeli <aysegovdeli@gmail.com>
2011-10-10 13:22:17 +0000

    galeri uygulamasi ornegi yapildi

--- ayse/src/com/android/AndroidUygulamasiActivity.java
+++ ayse/src/com/android/AndroidUygulamasiActivity.java
@@ -5,12 +5,26 @@ import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
+import android.widget.Toast;
 
 public class AndroidUygulamasiActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+        setContentView(R.layout.main);     
+   
+    GridView gridview = (GridView) findViewById(R.id.gridview);
+    gridview.setAdapter(new ImageAdapter(this));
+
+    gridview.setOnItemClickListener(new OnItemClickListener() {
+        public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+            Toast.makeText(AndroidUygulamasiActivity.this, "" + position, Toast.LENGTH_SHORT).show();
+        }
+    });
+     
+}
 }
