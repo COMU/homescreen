e553f91b4c2236d28c870f70e1b90b8c425e3aba
simgeszgn <simgesezgin88@gmail.com>
2011-10-10 12:39:28 +0000

    guncelleme denemesi

--- simge/AndroidUygulamasi/src/com/example/android/AndroidUygulamasiActivity.java
+++ simge/AndroidUygulamasi/src/com/example/android/AndroidUygulamasiActivity.java
@@ -2,12 +2,15 @@ package com.example.android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class AndroidUygulamasiActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        Button buton = new Button(this);
+        buton.setText("basiniz");
+        setContentView(buton);
     }
 }
