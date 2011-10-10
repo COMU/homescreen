f8a91adbef2549f3185b34c3d126724e3c91dd96
damlaok <damla.ok.90@gmail.com>
2011-10-10 11:28:38 +0000

    resim ekleme

---
+++ damla/resimEkle/src/com/example/resim/ResimEkleActivity.java
@@ -0,0 +1,17 @@
+package com.example.resim;
+
+
+
+import android.app.Activity;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+
+public class ResimEkleActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.anasayfa);
+        
+    }
+}
