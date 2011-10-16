edaacca1b91654ed005630d8031a82336dca87cf
mehmetcetin <mehmetcetin07.89@gmail.com>
2011-10-16 16:37:01 +0000

    hem giris yapıp hem tewetleme denemesi

--- Mehmet/Twitter/src/com/comu/homescreen/TwitterActivity.java
+++ Mehmet/Twitter/src/com/comu/homescreen/TwitterActivity.java
@@ -1,7 +1,17 @@
 package com.comu.homescreen;
 
+import winterwell.jtwitter.Twitter;
+import winterwell.jtwitter.TwitterException;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class TwitterActivity extends Activity {
@@ -13,8 +23,44 @@ public class TwitterActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+        EditText username=(EditText)findViewById(com.comu.homescreen.R.id.UserName);
+        EditText pwd=(EditText)findViewById(com.comu.homescreen.R.id.passwd);
+        String Name=username.getText().toString();
+        String Pwd=pwd.getText().toString();
+      Button submit =(Button)findViewById(com.comu.homescreen.R.id.submit);
+      
         
-        
-        
+     final Twitter myTwit = new Twitter(Name,Pwd);
+   
+submit.setOnClickListener(new OnClickListener() {
+	
+	@Override
+	public void onClick(View v) {
+		
+		
+		
+		 try
+	     {
+	     
+	     myTwit.setStatus("This is my first Tweet from Android ");
+	     Toast.makeText(TwitterActivity.this, "Buda ilk tweet olsun Android emulatorden :)", Toast.LENGTH_SHORT).show();
+	     }
+	     catch(TwitterException.E401 e)
+	     {
+	     // comes here when username or password is wrongs
+	     Toast.makeText(TwitterActivity.this, " Giris Basarisiz ",Toast.LENGTH_SHORT).show();
+	     }
+	     catch(Exception e)
+	     {
+	     Toast.makeText(TwitterActivity.this, " Agda sorun var",Toast.LENGTH_SHORT).show();
+	     }
+	     
+		
+	}
+});
+     
+     
+     
     }
+     
 }
