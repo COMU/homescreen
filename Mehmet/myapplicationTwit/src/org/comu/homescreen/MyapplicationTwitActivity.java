d4fb540951f374e86d88ac6c0bcb92a626dcb464
mehmetcetin <mehmetcetin07.89@gmail.com>
2011-10-17 20:19:30 +0000

    COnstant sınıfı eklendi

---
+++ Mehmet/myapplicationTwit/src/org/comu/homescreen/MyapplicationTwitActivity.java
@@ -0,0 +1,16 @@
+package org.comu.homescreen;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyapplicationTwitActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        
+        
+    }
+}
