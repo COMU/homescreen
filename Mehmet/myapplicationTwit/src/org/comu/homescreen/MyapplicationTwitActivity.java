f002c63c6d1bfee4ba20d1b65e96709c6fec27ac
mehmetcetin <mehmetcetin07.89@gmail.com>
2011-10-18 10:56:55 +0000

    ana Activity

--- Mehmet/myapplicationTwit/src/org/comu/homescreen/MyapplicationTwitActivity.java
+++ Mehmet/myapplicationTwit/src/org/comu/homescreen/MyapplicationTwitActivity.java
@@ -1,16 +1,37 @@
 package org.comu.homescreen;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.preference.PreferenceManager;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MyapplicationTwitActivity extends Activity {
-    /** Called when the activity is first created. */
+    private SharedPreferences prefs;
+    private final Handler mTwitterHandler = new Handler();
+    private TextView loginStatus;
+	
+    final Runnable mUpdateTwitterNotification = new Runnable() {
+		
+		@Override
+		public void run() {
+			
+			Toast.makeText(getBaseContext(), "Tweet sent !", Toast.LENGTH_LONG).show();
+			
+			
+		}
+	};
+    
+    
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        
+        this.prefs=PreferenceManager.getDefaultSharedPreferences(this);
         
         
         
