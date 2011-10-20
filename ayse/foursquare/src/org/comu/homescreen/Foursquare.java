e36e7a1a0f4c9cca685a15c20021cbc1ba37cca1
aysegovdeli <aysegovdeli@gmail.com>
2011-10-20 22:24:45 +0000

    http kimlik dogrulama istegi calismasi yapildi

--- ayse/foursquare/src/org/comu/homescreen/Foursquare.java
+++ ayse/foursquare/src/org/comu/homescreen/Foursquare.java
@@ -1,67 +1,41 @@
 package org.comu.homescreen;
 
 import java.io.IOException;
-
-import foursquare4j.exception.FoursquareException;
-import foursquare4j.oauth.FoursquareOAuthImpl;
-import foursquare4j.oauth.OAuthConsumer;
-import foursquare4j.type.Checkins;
-import foursquare4j.type.Credentials;
-import java.util.ArrayList;
-
-public class Foursquare extends Controller {
-
-    static final String FOURSQUARE_OAUTH_REQUEST_TOKEN = "http://foursquare.com/oauth/request_token";
-    static final String FOURSQUARE_OAUTH_ACCESS_TOKEN = "http://foursquare.com/oauth/access_token";
-    static final String FOURSQUARE_OAUTH_AUTHORIZE = "http://foursquare.com/oauth/authorize";
-    static final String CONSUMER_KEY = "PGHS1VJAVPPYJTP5ZQR5JOEHWWNDHOUOCGWRIHFZIS3ZXZSB";
-    static final String CONSUMER_SECRET = "OVBN15MAQL2X3FROAHGNGGKNMUC5T3ZBB0S2KOQX2TP05J3E";
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import fi.foyt.foursquare.api.FoursquareApi;
+import fi.foyt.foursquare.api.FoursquareApiException;
 
 
-    public static void authorize() throws OAuthMessageSignerException, OAuthNotAuthorizedException, OAuthExpectationFailedException, OAuthCommunicationException {
+public class Foursquare {
+	
+	String consumerKey = "PGHS1VJAVPPYJTP5ZQR5JOEHWWNDHOUOCGWRIHFZIS3ZXZSB";
+	String consumerSecret = "OVBN15MAQL2X3FROAHGNGGKNMUC5T3ZBB0S2KOQX2TP05J3E";
 
-        oauth.signpost.OAuthProvider provider = new DefaultOAuthProvider(FOURSQUARE_OAUTH_REQUEST_TOKEN, FOURSQUARE_OAUTH_ACCESS_TOKEN,FOURSQUARE_OAUTH_AUTHORIZE);
-        oauth.signpost.OAuthConsumer consumer = new DefaultOAuthConsumer(CONSUMER_KEY,CONSUMER_SECRET);
-        String authURL;
-
-        authURL = provider.retrieveRequestToken(consumer,"");
-
-        String tokenSecret = consumer.getTokenSecret();
-        session.put("secret", tokenSecret);
-        redirect(authURL);
-    }
-
-    
-    public static void oauth()
-    {
-
-        oauth.signpost.OAuthProvider provider = new DefaultOAuthProvider(FOURSQUARE_OAUTH_REQUEST_TOKEN, FOURSQUARE_OAUTH_ACCESS_TOKEN,FOURSQUARE_OAUTH_AUTHORIZE);
-        oauth.signpost.OAuthConsumer consumer = new DefaultOAuthConsumer(CONSUMER_KEY,CONSUMER_SECRET);
-        String secret = session.get("secret");
-        String pinCode = params.get("oauth_token");
-        consumer.setTokenWithSecret(pinCode, secret);
-        provider.retrieveAccessToken(consumer, pinCode);
-
-      
-        String token = consumer.getToken();
-        String tokenSecret = consumer.getTokenSecret();
-
-       
-        foursquare4j.type.Credentials credentials = new Credentials();
-        credentials.setTokenSecret(tokenSecret);
-        credentials.setAccessToken(token);
-
-        foursquare4j.oauth.OAuthConsumer newConsumer = new OAuthConsumer(CONSUMER_KEY, CONSUMER_SECRET);
-
-        foursquare4j.oauth.FoursquareOAuthImpl fs = new FoursquareOAuthImpl(newConsumer, credentials);
-
-        try {
-           
-            Checkins checkins = fs.history("50", "");
-
-            render(checkins);
-        } catch (FoursquareException e) {
-            e.printStackTrace();
-        }
-    }
-}
+		  
+	  public void authenticationRequest(HttpServletRequest request, HttpServletResponse response) {
+		    FoursquareApi foursquareApi = new FoursquareApi(consumerKey, consumerKey, "Callback URL");
+		    try {
+		     
+		      response.sendRedirect(foursquareApi.getAuthenticationUrl());
+		    } catch (IOException e) {
+		     
+		    }
+		  }
+		  
+		  public void handleCallback(HttpServletRequest request, HttpServletResponse response) {
+		    
+		    FoursquareApi foursquareApi = new FoursquareApi("Client ID", "Client Secret", "Callback URL");
+		   
+		    String code = request.getParameter("code");
+		    try {
+		     
+		      foursquareApi.authenticateCode(code);
+		     
+		    } catch (FoursquareApiException e) {
+		     
+		    }
+		  }
+		  
+		}
+	
