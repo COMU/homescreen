655f00dff35926643b94cefc915564a15bd9365f
simgeszgn <simgesezgin88@gmail.com>
2011-10-31 11:17:50 +0000

    GTalk için XMPP client tarafinin yazilmasi

---
+++ simge/XMPPClient/src/org/comu/homescreen/SettingsDialog.java
@@ -0,0 +1,69 @@
+package org.comu.homescreen;
+
+import org.jivesoftware.smack.ConnectionConfiguration;
+import org.jivesoftware.smack.SASLAuthentication;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.packet.Presence;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class SettingsDialog extends Dialog implements  android.view.View.OnClickListener{
+	
+	private XMPPClientActivity xmppClient;
+
+    public SettingsDialog(XMPPClientActivity xmppClient) {
+        super(xmppClient);
+        this.xmppClient = xmppClient;
+    }
+    
+    protected void onStart() {
+        super.onStart();
+        setContentView(R.layout.settings);
+        getWindow().setFlags(4, 4);
+        setTitle("XMPP Settings");
+        Button ok = (Button) findViewById(R.id.ok);
+        ok.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        String username = getText(R.id.userid);
+        String password = getText(R.id.password);
+
+        // Create a connection
+        ConnectionConfiguration connConfig =
+                new ConnectionConfiguration("talk.google.com", 5222, "gmail.com");
+        connConfig.setSASLAuthenticationEnabled(false);
+        XMPPConnection connection = new XMPPConnection(connConfig);
+
+        try {
+            connection.connect();
+            SASLAuthentication.supportSASLMechanism("PLAIN", 0);
+            connection.login(username, password);
+            }
+        catch(XMPPException e1) {
+        	e1.printStackTrace();
+        	xmppClient.setConnection(null);
+        	}
+        try{
+        	connection.login(username, password);
+        }
+        catch(XMPPException ex){
+        	ex.printStackTrace();
+        	xmppClient.setConnection(null);
+        }
+       // dismiss();
+       
+    }
+
+    private String getText(int id) {
+        EditText widget = (EditText) this.findViewById(id);
+        return widget.getText().toString();
+    }
+}
