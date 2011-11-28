a70ac8ecb9f0e6e313178e58c2ff67df375a31bb
aysegovdeli <aysegovdeli@gmail.com>
2011-11-28 13:27:17 +0000

    xml deki hata duzeltildi.

--- ayse/NotDefteriWidget/gen/com/comu/android/R.java
+++ ayse/NotDefteriWidget/gen/com/comu/android/R.java
@@ -14,14 +14,16 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int widget_textview=0x7f050000;
+        public static final int widget_textview=0x7f060000;
     }
     public static final class layout {
-        public static final int hello_widget_provider=0x7f030000;
-        public static final int main=0x7f030001;
+        public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int widget_text=0x7f040000;
+        public static final int app_name=0x7f050001;
+        public static final int widget_text=0x7f050000;
+    }
+    public static final class xml {
+        public static final int hello_widget_provider=0x7f040000;
     }
 }
