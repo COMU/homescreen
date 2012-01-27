a6afad91f6c9633d717c5c8febf6fa6152d5448d
Ayse Govdeli <aysegovdeli@gmail.com>
2012-01-27 14:17:55 +0200

    İlk android uygulamasi depoya eklendi

---
+++ ayse/HelloAndroid/src/com/example/hello/HelloAndroidActivity.java
@@ -0,0 +1,19 @@
+package com.example.hello;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class HelloAndroidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+    
+    
+    
+    
+    
+}
