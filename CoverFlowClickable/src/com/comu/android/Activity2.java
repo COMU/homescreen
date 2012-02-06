88eb1e630858c69d92f04b81d15ea9c8e1fb7809
simgeszgn <simgesezgin88@gmail.com>
2012-02-06 11:16:13 +0200

    Anamenu icin iconlar belirlendi ve eklendi

---
+++ CoverFlowClickable/src/com/comu/android/Activity2.java
@@ -0,0 +1,14 @@
+package com.comu.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+	public class Activity2 extends Activity {
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.browser);   
+
+}
+}
---
+++ CoverFlowClickable/src/com/comu/android/CoverFlowClickableActivity.java
@@ -0,0 +1,199 @@
+package com.comu.android;
+
+import java.lang.reflect.Field;
+import com.comu.android.R;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.LinearGradient;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Bitmap.Config;
+import android.graphics.PorterDuff.Mode;
+import android.graphics.Shader.TileMode;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
+import android.widget.Toast;
+
+public class CoverFlowClickableActivity extends Activity implements OnItemClickListener{
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		final CoverFlow coverFlow;
+		coverFlow = new CoverFlow(this);
+		coverFlow.setOnItemClickListener(this);
+		coverFlow.setAdapter(new ImageAdapter(this));
+
+		ImageAdapter coverImageAdapter = new ImageAdapter(this);
+        
+		coverFlow.setAdapter(coverImageAdapter);
+
+		coverFlow.setSpacing(-30);
+		coverFlow.setSelection(4, true);
+		coverFlow.setAnimationDuration(1000);
+
+		setContentView(coverFlow);
+		
+	}
+	
+
+	public class ImageAdapter extends BaseAdapter {
+		int mGalleryItemBackground;
+		private Context mContext;
+
+		int[] mImageIds = getAllResourceIDs(R.drawable.class);
+
+		private int[] getAllResourceIDs(Class<?> aClass)
+				throws IllegalArgumentException {
+
+			Field[] IDFields = aClass.getFields();
+
+			int[] IDs = new int[IDFields.length - 1];
+
+			try {
+
+				for (int i = 0; i < IDFields.length - 1; i++) {
+					IDs[i] = IDFields[i + 1].getInt(null);
+				}
+			} catch (Exception e) {
+				throw new IllegalArgumentException();
+			}
+			return IDs;
+		}
+
+		private ImageView[] mImages;
+
+		public ImageAdapter(Context c) {
+			mContext = c;
+			mImages = new ImageView[mImageIds.length];
+		}
+
+		public boolean createReflectedImages() {
+
+			final int reflectionGap = 4;
+
+			int index = 0;
+			for (int imageId : mImageIds) {
+				Bitmap originalImage = BitmapFactory.decodeResource(
+						getResources(), imageId);
+				int width = originalImage.getWidth();
+				int height = originalImage.getHeight();
+
+				// This will not scale but will flip on the Y axis
+				Matrix matrix = new Matrix();
+				matrix.preScale(1, -1);
+
+				// Create a Bitmap with the flip matrix applied to it.
+				// We only want the bottom half of the image
+				Bitmap reflectionImage = Bitmap.createBitmap(originalImage, 0,
+						height / 2, width, height / 2, matrix, false);
+
+				// Create a new bitmap with same width but taller to fit
+				// reflection
+				Bitmap bitmapWithReflection = Bitmap.createBitmap(width,
+						(height + height / 2), Config.ARGB_8888);
+
+				// Create a new Canvas with the bitmap that's big enough for
+				// the image plus gap plus reflection
+				Canvas canvas = new Canvas(bitmapWithReflection);
+				// Draw in the original image
+				canvas.drawBitmap(originalImage, 0, 0, null);
+				// Draw in the gap
+				Paint deafaultPaint = new Paint();
+				canvas.drawRect(0, height, width, height + reflectionGap,
+						deafaultPaint);
+				// Draw in the reflection
+				canvas.drawBitmap(reflectionImage, 0, height + reflectionGap,
+						null);
+
+				// Create a shader that is a linear gradient that covers the
+				// reflection
+				Paint paint = new Paint();
+				LinearGradient shader = new LinearGradient(0,
+						originalImage.getHeight(), 0,
+						bitmapWithReflection.getHeight() + reflectionGap,
+						0x70ffffff, 0x00ffffff, TileMode.CLAMP);
+				// Set the paint to use this shader (linear gradient)
+				paint.setShader(shader);
+				// Set the Transfer mode to be porter duff and destination in
+				paint.setXfermode(new PorterDuffXfermode(Mode.DST_IN));
+				// Draw a rectangle using the paint with our linear gradient
+				canvas.drawRect(0, height, width,
+						bitmapWithReflection.getHeight() + reflectionGap, paint);
+
+				ImageView imageView = new ImageView(mContext);
+				imageView.setImageBitmap(bitmapWithReflection);
+				imageView.setLayoutParams(new CoverFlow.LayoutParams(240, 360));
+				imageView.setScaleType(ScaleType.MATRIX);
+				mImages[index++] = imageView;
+
+			}
+			return true;
+		}
+
+		public int getCount() {
+			return mImageIds.length;
+		}
+
+		public Object getItem(int position) {
+			return position;
+		}
+
+		public final long getItemId(int position) {
+			return position;
+		}
+
+		public View getView(int position, View convertView, ViewGroup parent) {
+
+			// Use this code if you want to load from resources
+			ImageView i = new ImageView(mContext);
+			i.setImageResource(mImageIds[position]);
+			i.setLayoutParams(new CoverFlow.LayoutParams(200, 300));
+			i.setScaleType(ImageView.ScaleType.FIT_XY);
+
+			// Make sure we set anti-aliasing otherwise we get jaggies
+			BitmapDrawable drawable = (BitmapDrawable) i.getDrawable();
+			drawable.setAntiAlias(true);
+			return i;
+
+			// return mImages[position];
+		}
+
+		/**
+		 * Returns the size (0.0f to 1.0f) of the views depending on the
+		 * 'offset' to the center.
+		 */
+		public float getScale(boolean focused, int offset) {
+			/* Formula: 1 / (2 ^ offset) */
+			return Math
+					.max(0, 1.0f / (float) Math.pow(1 / 2, Math.abs(offset)));
+		}
+	
+	}
+
+
+	@Override
+	public void onItemClick(AdapterView<?> Gallery, View arg1, int position, long arg3) {
+		// TODO Auto-generated method stub
+		Toast.makeText(	CoverFlowClickableActivity.this , "resime tiklandi "+ ++position +".resim", position).show();
+	//	int id=CoverFlow.FIND_VIEWS_WITH_TEXT;
+	//		if(position == 3) {
+	//			Intent intent = new Intent(getApplicationContext(), Activity2.class);
+	//			startActivity(intent);
+	//        } 
+
+	}
+}
