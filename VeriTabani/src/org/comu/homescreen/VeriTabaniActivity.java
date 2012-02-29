0d592bb16acd34398ef505e80f7cb32b1d6b4e92
Mehmet Cetin <mehmetcetin07.89@gmail.com>
2012-02-29 14:00:42 +0200

    Veri tabanından etiketle basarıyla cekildi

--- VeriTabani/src/org/comu/homescreen/VeriTabaniActivity.java
+++ VeriTabani/src/org/comu/homescreen/VeriTabaniActivity.java
@@ -18,94 +18,97 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class VeriTabaniActivity extends Activity{	
+public class VeriTabaniActivity extends Activity {
 	private VeriTabani imagepath;
-	
-	protected void onCreate(Bundle savedInstanceState){
+
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        imagepath = new VeriTabani(this);
-//        final EditText yol = (EditText)findViewById(R.id.imagepath);
-      
-        Button verigonder = (Button)findViewById(R.id.buton);
-        verigonder.setOnClickListener(new View.OnClickListener(){
-        				
+		setContentView(R.layout.main);
+
+		imagepath = new VeriTabani(this);
+
+		Button verigonder = (Button) findViewById(R.id.buton);
+		verigonder.setOnClickListener(new View.OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				
-		  Integer[]  images = {
-					        R.drawable.sample_1,
-					        R.drawable.sample_1
-					    
-					        		};
-				
-		try{
-				ArrayList todoItems = new ArrayList();
-		    	todoItems.add("0x7f020001");
-	
-		    //	for(int i=0; i< todoItems.size();i++){
-		    	KayitEkle(images[0].toString(),"browser");
-		  //  	KayitEkle(images[1].toString(),"resim_2");
-		    		//KayitEkle(todoItems.get(i).toString());	
-			//	}
+
+				Integer[] images = { 
+						R.drawable.sample_1,
+						R.drawable.sample_2
+						};
+
+				try {
+
+					KayitEkle(images[0].toString(), "browser");
+					KayitEkle(images[1].toString(), "gtalk");
+
+				} finally {
+					imagepath.close();
 				}
-			finally{
-				imagepath.close();
-			}
 			}
 
 			private void KayitEkle(String yol, String etiket) {
 				// TODO Auto-generated method stub
-		
-				
-				
+
 				SQLiteDatabase db = imagepath.getWritableDatabase();
 				ContentValues veriler = new ContentValues();
 				veriler.put("imagepath", yol);
 				veriler.put("etiket", etiket.toString());
-				
+
 				db.insertOrThrow("temacesitleri", null, veriler);
-				
+
 			}
-		
-        });
-        
-        Button temadegistir = (Button) findViewById(R.id.ikincibuton);
-        temadegistir.setOnClickListener(new View.OnClickListener() {
-			
+
+		});
+
+		Button temadegistir = (Button) findViewById(R.id.ikincibuton);
+		temadegistir.setOnClickListener(new View.OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				try{
+				try {
 					Cursor cursor = KayitGetir();
 					KayitGoster(cursor);
-				}
-				finally{
+				} finally {
 					imagepath.close();
-					
+
 				}
 			}
-			private String [] SELECT = {"imagepath"};
-			
+
+			private String[] SELECT = { "imagepath","etiket" };
+
 			private Cursor KayitGetir() {
 				// TODO Auto-generated method stub
 				SQLiteDatabase db = imagepath.getReadableDatabase();
-				Cursor cursor = db.query("temacesitleri", SELECT, null, null, null, null, null);
+				Cursor cursor = db.query("temacesitleri", SELECT, null, null,
+						null, null, null);
 				startManagingCursor(cursor);
 				return cursor;
 			}
-			
-			private void KayitGoster(Cursor cursor){
-				while(cursor.moveToNext()){			
-					String yol_adi = cursor.getString(cursor.getColumnIndex("imagepath"));	
-					ImageView img = (ImageView)findViewById(R.id.img);
-			Bitmap bm = BitmapFactory.decodeResource(getResources(), Integer.parseInt(yol_adi));
+
+			private void KayitGoster(Cursor cursor) {
+				while (cursor.moveToNext()) {
+					String yol_adi = cursor.getString(cursor
+							.getColumnIndex("imagepath"));
+					
+					String etiket=cursor.getString(cursor.getColumnIndex("etiket"));
+					TextView t= (TextView) findViewById(R.id.text);
+					t.setText(etiket);
+					
+					ImageView img = (ImageView) findViewById(R.id.img);
+					Bitmap bm = BitmapFactory.decodeResource(getResources(),
+							Integer.parseInt(yol_adi));
 					img.setImageBitmap(bm);
+				
+					
 				}
 			}
-		});       
-      } 
+			
+			
+			
+		});
 	}
-
-
+}
