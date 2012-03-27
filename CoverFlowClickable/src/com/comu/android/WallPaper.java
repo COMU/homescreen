03da76771ca693494dd5ed3501073b2a241b13b6
Ayse Govdeli <aysegovdeli@gmail.com>
2012-03-28 00:36:10 +0300

    wallpaper sinifi olusturuldu

---
+++ CoverFlowClickable/src/com/comu/android/WallPaper.java
@@ -0,0 +1,12 @@
+package com.comu.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WallPaper  extends Activity{
+	
+	protected void onCreate(Bundle savedInstanceState) {
+		// Called when the activity is first created
+		super.onCreate(savedInstanceState);
+	}
+}
