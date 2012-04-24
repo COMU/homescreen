f2c42074c17e56e84515503732ff42c312fbe7b0
Simge Sezgin <simgesezgin88@gmail.com>
2012-04-25 01:44:47 +0300

    GTalk icin Login sayfasi hazirlandi

---
+++ simge/GTalk/src/com/comu/android/GTalkActivity.java
@@ -0,0 +1,13 @@
+package com.comu.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GTalkActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
