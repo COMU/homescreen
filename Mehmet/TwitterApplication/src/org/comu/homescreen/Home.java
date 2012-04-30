7bca2eaf43ac242445a30249faa0321ff5d34ba5
Mehmet Cetin <mehmetcetin07.89@gmail.com>
2012-04-30 10:13:40 +0300

    twitter uygulamsı olusturuldu ve autotentication islemi uygulama baslatılınca calısır hala getırıldı

---
+++ Mehmet/TwitterApplication/src/org/comu/homescreen/Home.java
@@ -0,0 +1,14 @@
+package org.comu.homescreen;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class Home extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+}
