39608436eaee682f3ef706f5418773cbad9479ae
Simge Sezgin <simgesezgin88@gmail.com>
2012-05-17 04:32:25 +0300

    Arkadas listesine tiklama eklendi

--- simge/XMPPClient/src/org/comu/homescreen/XMPPClientActivity.java
+++ simge/XMPPClient/src/org/comu/homescreen/XMPPClientActivity.java
@@ -16,14 +16,22 @@ import org.jivesoftware.smack.packet.Presence;
 import org.jivesoftware.smack.util.StringUtils;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class XMPPClientActivity extends Activity {
     /** Called when the activity is first created. */
@@ -45,7 +53,7 @@ public class XMPPClientActivity extends Activity {
         setContentView(R.layout.main);
              
 //        mSendText = (EditText) this.findViewById(R.id.sendText);
-        mList = (ListView) this.findViewById(R.id.listMessages);
+//        mList = (ListView) this.findViewById(R.id.listMessages);
         mFriend = (ListView) this.findViewById(R.id.friendList);
 //        setListAdapter();
 //        setFriendAdapter();
@@ -56,7 +64,7 @@ public class XMPPClientActivity extends Activity {
      // Set a listener to show the settings dialog
         final Button login = (Button) this.findViewById(R.id.login);
         login.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
+            public void onClick(View view) {       	
             	login.setVisibility(View.GONE);
                 mHandler.post(new Runnable() {
                     public void run() {
@@ -68,12 +76,10 @@ public class XMPPClientActivity extends Activity {
         });
 
         
-        // Set a listener to send a chat text message
+//        // Set a listener to send a chat text message
 //        Button send = (Button) this.findViewById(R.id.send);
 //        send.setOnClickListener(new View.OnClickListener() {
 //            public void onClick(View view) {
-//                String to = mRecipient.getText().toString();
-//                String text = mSendText.getText().toString();
 //
 //                Log.i("XMPPClient", "Sending text [" + text + "] to [" + to + "]");
 //                Message msg = new Message(to, Message.Type.chat);
@@ -116,26 +122,40 @@ public class XMPPClientActivity extends Activity {
             }, filter); 	
             
            
-            ListView l= (ListView) findViewById(org.comu.homescreen.R.id.friendList);
+            final ListView l= (ListView) findViewById(org.comu.homescreen.R.id.friendList);
             		
-            ArrayList<RosterEntry>listFriend = new ArrayList<RosterEntry>();
+            final ArrayList<RosterEntry>listFriend = new ArrayList<RosterEntry>();
 			Roster roster = connection.getRoster();
 			Collection<RosterEntry> entries = roster.getEntries();
 			for(RosterEntry entry : entries){
 				listFriend.add(entry);				
 				Log.v("DEBUG", entry.toString());
 		}
-			String[] tmpList= new String[listFriend.size()];
+	
+			final String[] tmpList= new String[listFriend.size()];
 			for(int m=0;m<listfriend.size();m++){
 	    		tmpList[m] = listfriend.get(m).getName().toString();
 	    	}
 			
 			l.setAdapter(new ArrayAdapter<RosterEntry>(l.getContext(), android.R.layout.simple_list_item_1, listFriend));
 			
+			l.setOnItemClickListener(new OnItemClickListener() {
+
+				@Override
+				public void onItemClick(AdapterView<?> av, View arg1,
+						int arg2, long arg3) {
+					// TODO Auto-generated method stub
+							TextView side = (TextView) findViewById(R.id.karsitaraf);
+//							side.setText(tmpList[arg2]);
+							RosterEntry friend = (RosterEntry)l.getItemAtPosition(arg2);
+							side.setText(friend.toString() + "ile konusuluyor...");
+				}
+			});
+		
+						
         }
     }
     
-
 
     private void setListAdapter() {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
