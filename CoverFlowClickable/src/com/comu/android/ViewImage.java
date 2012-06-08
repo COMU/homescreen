4347169e7cd5d07473751a86d1a161e7850015a0
Damla Ok <damla.ok.90@gmail.com>
2012-06-08 14:08:37 +0300

    galeri altindaki resimler tiklandiginda tam ekran olarak goruntuleniyor

---
+++ CoverFlowClickable/src/com/comu/android/ViewImage.java
@@ -0,0 +1,27 @@
+package com.comu.android;
+
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+public class ViewImage extends Activity {
+      private String filename;
+      @Override
+      public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            System.gc();
+            Intent i = getIntent();
+            Bundle extras = i.getExtras();
+            BitmapFactory.Options bfo = new BitmapFactory.Options();
+            bfo.inSampleSize = 2;
+            filename = extras.getString("filename");
+            ImageView iv = new ImageView(getApplicationContext());
+            Bitmap bm = BitmapFactory.decodeFile(filename, bfo);
+            iv.setImageBitmap(bm);
+            setContentView(iv);
+      } 
+}
